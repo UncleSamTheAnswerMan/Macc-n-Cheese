9c9575df232878cfe3db44731f182edb5d7e69a8
Sam <heyi1sam@live.bemidjistate.edu>
2016-03-30 08:31:09 -0500

    added stuff to mncCode

--- mncCode.h
+++ mncCode.h
@@ -4,5 +4,27 @@
 
 #ifndef MACC_N_CHEESE_MNCCODE_H
 #define MACC_N_CHEESE_MNCCODE_H
+#include <string>
+
+using namespace std;
+
+enum shoutType {floatType, intType, stringType};
+
+struct ShoutExpr {
+    shoutType theType;
+    int intVal;
+    string stringVal;
+    float floatVal;
+};
+
+class CodeGen{
+public:
+    void Shout(ShoutExpr shoutStuff);
+
+    void ProcessLit();
+
+private:
+
+};
 
 #endif //MACC_N_CHEESE_MNCCODE_H
