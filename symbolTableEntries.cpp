d6d125f90dbefd6a34a0962dbd8d799a46cf154a
Sam <heyi1sam@live.bemidjistate.edu>
2016-04-06 14:56:32 -0500

    put functions and stuff into symbolTableEntries

--- symbolTableEntries.cpp
+++ symbolTableEntries.cpp
@@ -3,3 +3,47 @@
 //
 
 #include "symbolTableEntries.h"
+
+string symbolTableEntries::getName() {
+    return name;
+}
+
+symbolTableEntries::DataType symbolTableEntries::getDataType() {
+    return dataType;
+}
+
+int symbolTableEntries::getRelAddress() {
+    return relativeAddress;
+}
+
+bool symbolTableEntries::getHipHip() {
+    return hiphip;
+}
+
+int symbolTableEntries::getNumComponents() {
+    return numComponents;
+}
+
+vector<string> symbolTableEntries::getInitValues() {
+    return initValues;
+}
+
+void symbolTableEntries::setName(string n) {
+    name = n;
+}
+
+void symbolTableEntries::setDataType(DataType d) {
+    dataType = d;
+}
+
+void symbolTableEntries::setHipHip(bool h) {
+    hiphip = h;
+}
+
+void symbolTableEntries::setNumComponents(int n) {
+    numComponents = n;
+}
+
+void symbolTableEntries::setInitVals(vector<string> i) {
+    initValues = i;
+}
